83f6c9889fe0302d631529b5dd250cdd41de44d8
John Tsevdos <tsevdosjohn@gmail.com>
2021-06-01 10:31:09 +0300

    Todo app

--- intermediate/workshop/todo-app/src/TodoApp.js
+++ intermediate/workshop/todo-app/src/TodoApp.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./components/TodoItem";
 
 const TodoApp = ({ initialTodos }) => {
+  const [toDoInput, setToDoInput] = useState("");
+  const [todos, setTodos] = useState(initialTodos);
+
+  const updateInput = (e) => {
+    setToDoInput(e.target.value);
+  };
+
+  const addToDo = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      title: toDoInput,
+      done: false,
+    };
+
+    setTodos([...todos, newTodo]);
+    setToDoInput("");
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].done = !newTodos[index].done;
+
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index) => {
+    console.log("delete todo", index);
+  };
+
   return (
     <div>
       <h2>My ToDos</h2>
-      <form>
-        Add ToDo: <input type="text" />
+      <form onSubmit={addToDo}>
+        Add ToDo: <input type="text" value={toDoInput} onChange={updateInput} />
         <button type="submit">Add ToDo</button>
       </form>
       <ul>
-        {initialTodos.map((todo, index) => (
-          <TodoItem key={todo.title} {...todo} index={index} />
+        {todos.map((todo, index) => (
+          <TodoItem
+            key={todo.title}
+            {...todo}
+            index={index}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
         ))}
       </ul>
     </div>
--- intermediate/workshop/todo-app/src/components/TodoItem.js
+++ intermediate/workshop/todo-app/src/components/TodoItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-const TodoItem = ({ title, done }) => {
+const TodoItem = ({ title, done, index, toggleTodo, deleteTodo }) => {
   return (
     <li>
       <label>
-        <input type="checkbox" checked={done} />
+        <input
+          type="checkbox"
+          checked={done}
+          onChange={() => toggleTodo(index)}
+        />
         &nbsp;
         {done ? <strike>{title}</strike> : <span>{title}</span>}
+        <button onClick={() => deleteTodo(index)}>delete</button>
       </label>
     </li>
   );
